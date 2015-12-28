74a74efa35e54f4c06825349cf8d8be12b7f1cfc
brianpope-pf <brian.pope@peoplefluent.com>
2015-12-28 14:31:46 -0700

    first commit

---
+++ src/MCM/Extensions/RequestExtension.cs
@@ -0,0 +1,27 @@
+﻿namespace MCM.Extensions
+{
+    using System.Web;
+
+    public static class RequestExtension
+    {
+        public static bool IsAjaxRequest(this HttpRequestBase request)
+        {
+            if (request == null)
+            {
+                return false;
+            }
+
+            if (request["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return true;
+            }
+
+            if (request.Headers != null)
+            {
+                return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/MCM/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MCM.Startup))]
+
+namespace MCM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
