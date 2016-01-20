989663e947c368230f8cc9a0b73472fa775ee73e
Brian Pope <brianlpope@gmail.com>
2016-01-19 21:10:58 -0500

    style updates

--- src/MCM/App_Start/BundleConfig.cs
+++ src/MCM/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/bootstrap-override.css"));
+                      "~/Content/bootstrap-override.css",
+                      "~/Content/site.css"));
         }
     }
 }
